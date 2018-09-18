572ea4da7f1678c7e263dea5e0698392f76ee15f
Nicky Liu <nickyliu91@gmail.com>
2018-09-18 03:27:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    string = params["string"]
-    @reversed = string.reverse
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
 
     erb :reversed
   end
